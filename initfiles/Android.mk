fec35e5b80c29ea58176cddb528a3e43a27a8cef
Aaron Kling <webgeek1234@gmail.com>
2019-03-20 15:29:33 -0500

    Add uevent
    Copied from t210 and modified for t19x addresses
    
    Change-Id: Ia730a4f42001353dfe81e678c5581ae761b44627

--- initfiles/Android.mk
+++ initfiles/Android.mk
@@ -15,3 +15,11 @@ LOCAL_SRC_FILES            := init.t19x_common.rc
 LOCAL_VENDOR_MODULE        := true
 LOCAL_MODULE_RELATIVE_PATH := init/hw
 include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE       := ueventd.t194ref.rc
+LOCAL_MODULE_STEM  := ueventd.rc
+LOCAL_MODULE_CLASS := ETC
+LOCAL_SRC_FILES    := ueventd.t194ref.rc
+LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR)
+include $(BUILD_PREBUILT)
