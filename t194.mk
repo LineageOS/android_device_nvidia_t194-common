1402534458cec1fee48c1390f8fbac109ee3d752
Aaron Kling <webgeek1234@gmail.com>
2019-03-19 23:38:38 -0500

    Add system properties
    Based on stock fosters build.prop since t194 has no public android
    devices or source.
    
    Change-Id: Ia0d071c49ed2c1a4bd43e08c0aad8d46229a0211

--- t194.mk
+++ t194.mk
@@ -17,6 +17,9 @@
 TARGET_TEGRA_VERSION := t194
 TARGET_TEGRA_GPU     ?= nvgpu
 
+# System properties
+include $(LOCAL_PATH)/system_prop.mk
+
 PRODUCT_PACKAGES += \
     init.t19x.rc \
     init.t19x_common.rc \
