LineageOS/android_device_nvidia_t210-common

Move ueventd conf to vendor etc folder
system/core removed support for having the conf in /vendor/ in
commit 71dd706.

Change-Id: I4a3648d168c9f8cdf43f90cdf244f4d2dcda1e87

initfiles/Android.mk
@@ -17,9 +17,9 @@ LOCAL_MODULE_RELATIVE_PATH := init/hw
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
-LOCAL_MODULE       := ueventd.t210ref.rc
-LOCAL_MODULE_STEM  := ueventd.rc
-LOCAL_MODULE_CLASS := ETC
-LOCAL_SRC_FILES    := ueventd.t210ref.rc
-LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR)
+LOCAL_MODULE        := ueventd.t210ref.rc
+LOCAL_MODULE_STEM   := ueventd.rc
+LOCAL_MODULE_CLASS  := ETC
+LOCAL_SRC_FILES     := ueventd.t210ref.rc
+LOCAL_VENDOR_MODULE := true
 include $(BUILD_PREBUILT)

LineageOS/android_device_nvidia_t194-common

initfiles/Android.mk
@@ -17,9 +17,9 @@ LOCAL_MODULE_RELATIVE_PATH := init/hw
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
-LOCAL_MODULE       := ueventd.t194ref.rc
-LOCAL_MODULE_STEM  := ueventd.rc
-LOCAL_MODULE_CLASS := ETC
-LOCAL_SRC_FILES    := ueventd.t194ref.rc
-LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR)
+LOCAL_MODULE        := ueventd.t194ref.rc
+LOCAL_MODULE_STEM   := ueventd.rc
+LOCAL_MODULE_CLASS  := ETC
+LOCAL_SRC_FILES     := ueventd.t194ref.rc
+LOCAL_VENDOR_MODULE := true
 include $(BUILD_PREBUILT)