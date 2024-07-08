dc1f4a52e7421c146ba75c2b971d94372c68aa5d
Aaron Kling <webgeek1234@gmail.com>
2024-07-07 22:34:28 -0500

    Don't use post install cmd for symlinks
    Deterministic partition images doesn't work right with them
    
    Change-Id: I018d744352e882b3603f58bf3c2aa24742238f27

--- vendor/r35/firmware/Android.mk
+++ vendor/r35/firmware/Android.mk
@@ -23,8 +23,7 @@ LOCAL_MODULE_CLASS         := ETC
 LOCAL_MODULE_PATH          := $(TARGET_OUT_VENDOR)/firmware/nvidia/tegra194
 LOCAL_MODULE_TAGS          := optional
 LOCAL_MODULE_OWNER         := nvidia
-LOCAL_POST_INSTALL_CMD     := \
-    ln -sf ./nvidia/tegra194/xusb.bin $(TARGET_OUT_VENDOR)/firmware/tegra19x_xusb_firmware
+LOCAL_REQUIRED_MODULES     := tegra19x_xusb_firmware_symlink
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
--- vendor/r35/t194.mk
+++ vendor/r35/t194.mk
@@ -16,6 +16,8 @@ LOCAL_PATH := device/nvidia/t194-common/vendor/r35
 
 include $(LOCAL_PATH)/t194-recovery.mk
 
+PRODUCT_SOONG_NAMESPACES += $(LOCAL_PATH)
+
 # Xusb firmware
 PRODUCT_PACKAGES += \
     xusb.bin
