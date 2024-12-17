192ca198120cd388f5cf23945e75fb7f4e37bea0
Aaron Kling <webgeek1234@gmail.com>
2024-12-16 22:43:35 -0600

    vendor: Use copy-files for firmware targets
    There's no real build time checks that the build system can do for these
    type of files. Plus duplicate copy-files entries are now a fatal error,
    so there's no benefit to keeping these as modules.
    
    Change-Id: I369008d4d6ed16b61ae56a7c7882993602fca70e

--- vendor/r35/t194.mk
+++ vendor/r35/t194.mk
@@ -1,4 +1,4 @@
-# Copyright (C) 2020 The LineageOS Project
+# Copyright (C) 2020-2024 The LineageOS Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -12,39 +12,54 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+
 LOCAL_PATH := device/nvidia/t194-common/vendor/r35
-
-include $(LOCAL_PATH)/t194-recovery.mk
-
 PRODUCT_SOONG_NAMESPACES += $(LOCAL_PATH)
 
-# Xusb firmware
-PRODUCT_PACKAGES += \
-    xusb.bin
+T194_FIRMWARE_PATH := vendor/nvidia/t194/r35/firmware
 
 # GPU firmware
-PRODUCT_PACKAGES += \
-    acr_ucode_dbg \
-    acr_ucode_prod \
-    fecs \
-    fecs_sig \
-    gpccs \
-    gpccs_sig \
-    gpmu_ucode \
-    gpmu_ucode_desc \
-    gpmu_ucode_image \
-    NETA_img \
-    NETB_img \
-    NETC_img \
-    NETD_img \
-    pmu_bl \
-    pmu_sig
+PRODUCT_COPY_FILES += \
+    $(T194_FIRMWARE_PATH)/gv11b/acr_ucode_dbg.bin:$(TARGET_COPY_OUT_VENDOR)/firmware/gv11b/acr_ucode_dbg.bin \
+    $(T194_FIRMWARE_PATH)/gv11b/acr_ucode_prod.bin:$(TARGET_COPY_OUT_VENDOR)/firmware/gv11b/acr_ucode_prod.bin \
+    $(T194_FIRMWARE_PATH)/gv11b/fecs.bin:$(TARGET_COPY_OUT_VENDOR)/firmware/gv11b/fecs.bin \
+    $(T194_FIRMWARE_PATH)/gv11b/fecs_sig.bin:$(TARGET_COPY_OUT_VENDOR)/firmware/gv11b/fecs_sig.bin \
+    $(T194_FIRMWARE_PATH)/gv11b/gpccs.bin:$(TARGET_COPY_OUT_VENDOR)/firmware/gv11b/gpccs.bin \
+    $(T194_FIRMWARE_PATH)/gv11b/gpccs_sig.bin:$(TARGET_COPY_OUT_VENDOR)/firmware/gv11b/gpccs_sig.bin \
+    $(T194_FIRMWARE_PATH)/gv11b/gpmu_ucode.bin:$(TARGET_COPY_OUT_VENDOR)/firmware/gv11b/gpmu_ucode.bin \
+    $(T194_FIRMWARE_PATH)/gv11b/gpmu_ucode_desc.bin:$(TARGET_COPY_OUT_VENDOR)/firmware/gv11b/gpmu_ucode_desc.bin \
+    $(T194_FIRMWARE_PATH)/gv11b/gpmu_ucode_image.bin:$(TARGET_COPY_OUT_VENDOR)/firmware/gv11b/gpmu_ucode_image.bin \
+    $(T194_FIRMWARE_PATH)/gv11b/NETA_img.bin:$(TARGET_COPY_OUT_VENDOR)/firmware/gv11b/NETA_img.bin \
+    $(T194_FIRMWARE_PATH)/gv11b/NETB_img.bin:$(TARGET_COPY_OUT_VENDOR)/firmware/gv11b/NETB_img.bin \
+    $(T194_FIRMWARE_PATH)/gv11b/NETC_img.bin:$(TARGET_COPY_OUT_VENDOR)/firmware/gv11b/NETC_img.bin \
+    $(T194_FIRMWARE_PATH)/gv11b/NETD_img.bin:$(TARGET_COPY_OUT_VENDOR)/firmware/gv11b/NETD_img.bin \
+    $(T194_FIRMWARE_PATH)/gv11b/pmu_bl.bin:$(TARGET_COPY_OUT_VENDOR)/firmware/gv11b/pmu_bl.bin \
+    $(T194_FIRMWARE_PATH)/gv11b/pmu_sig.bin:$(TARGET_COPY_OUT_VENDOR)/firmware/gv11b/pmu_sig.bin
 
 # General firmware
+PRODUCT_COPY_FILES += \
+    $(T194_FIRMWARE_PATH)/tegra19x/nvhost_nvdec040_ns.fw:$(TARGET_COPY_OUT_VENDOR)/firmware/tegra19x/nvhost_nvdec040_ns.fw \
+    $(T194_FIRMWARE_PATH)/tegra19x/nvhost_nvenc070.fw:$(TARGET_COPY_OUT_VENDOR)/firmware/tegra19x/nvhost_nvenc070.fw \
+    $(T194_FIRMWARE_PATH)/tegra19x/nvhost_nvenc070.fw:$(TARGET_COPY_OUT_VENDOR)/firmware/tegra19x/nvhost_nvenc070.fw \
+    $(T194_FIRMWARE_PATH)/nvhost_nvdla010.fw:$(TARGET_COPY_OUT_VENDOR)/firmware/nvhost_nvdla010.fw \
+    $(T194_FIRMWARE_PATH)/nvhost_vic042.fw:$(TARGET_COPY_OUT_VENDOR)/firmware/nvhost_vic042.fw \
+    $(T194_FIRMWARE_PATH)/nvpva_010.fw:$(TARGET_COPY_OUT_VENDOR)/firmware/nvpva_010.fw
+
+# Xusb firmware
+PRODUCT_COPY_FILES += \
+    $(T194_FIRMWARE_PATH)/xusb/tegra19x_xusb_firmware:$(TARGET_COPY_OUT_VENDOR)/firmware/tegra194/xusb.bin
+
 PRODUCT_PACKAGES += \
-    nvhost_nvdec040_ns \
-    nvhost_nvdla010 \
-    nvhost_nvenc070 \
-    nvhost_nvjpg012 \
-    nvhost_vic042 \
-    nvpva_010
+    tegra19x_xusb_firmware_symlink
+
+
+# Recovery / Boot Ramdisk
+ifeq ($(TARGET_TEGRA_KERNEL),4.9)
+PRODUCT_COPY_FILES += \
+    $(T194_FIRMWARE_PATH)/xusb/tegra19x_xusb_firmware:recovery/root/lib/firmware/tegra19x_xusb_firmware \
+    $(T194_FIRMWARE_PATH)/xusb/tegra19x_xusb_firmware:$(TARGET_COPY_OUT_RAMDISK)/lib/firmware/tegra19x_xusb_firmware
+else
+PRODUCT_COPY_FILES += \
+    $(T194_FIRMWARE_PATH)/xusb/tegra19x_xusb_firmware:recovery/root/lib/firmware/nvidia/tegra194/xusb.bin \
+    $(T194_FIRMWARE_PATH)/xusb/tegra19x_xusb_firmware:$(TARGET_COPY_OUT_RAMDISK)/lib/firmware/nvidia/tegra194/xusb.bin
+endif
