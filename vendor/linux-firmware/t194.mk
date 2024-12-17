391db98463b0791cc46936c3793016350b475a79
Aaron Kling <webgeek1234@gmail.com>
2024-12-16 22:43:35 -0600

    vendor: Use copy-files for firmware targets
    There's no real build time checks that the build system can do for these
    type of files. Plus duplicate copy-files entries are now a fatal error,
    so there's no benefit to keeping these as modules.
    
    Change-Id: I369008d4d6ed16b61ae56a7c7882993602fca70e

--- vendor/linux-firmware/t194.mk
+++ vendor/linux-firmware/t194.mk
@@ -12,14 +12,21 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-LOCAL_PATH := device/nvidia/t194-common/vendor/linux-firmware
+T194_FIRMWARE_PATH := kernel/nvidia/linux-firmware
 
-include $(LOCAL_PATH)/t194-recovery.mk
+DOWNSTREAM_BRANCH    := r36
+T194_DOWNSTREAM_PATH := vendor/nvidia/t194/$(DOWNSTREAM_BRANCH)/firmware
 
 # General firmware
-PRODUCT_PACKAGES += \
-    nvdec.bin \
-    nvenc.bin \
-    nvjpg.bin \
-    vic.bin \
-    xusb.bin
+PRODUCT_COPY_FILES += \
+    $(T194_DOWNSTREAM_PATH)/tegra19x/nvhost_nvdec040_ns.fw:$(TARGET_COPY_OUT_VENDOR)/firmware/nvidia/tegra194/nvdec.bin \
+    $(T194_DOWNSTREAM_PATH)/tegra19x/nvhost_nvenc070.fw:$(TARGET_COPY_OUT_VENDOR)/firmware/nvidia/tegra194/nvenc.bin \
+    $(T194_DOWNSTREAM_PATH)/tegra19x/nvhost_nvjpg012.fw:$(TARGET_COPY_OUT_VENDOR)/firmware/nvidia/tegra194/nvjpg.bin \
+    $(T194_FIRMWARE_PATH)/nvidia/tegra194/vic.bin:$(TARGET_COPY_OUT_VENDOR)/firmware/nvidia/tegra194/vic.bin \
+    $(T194_FIRMWARE_PATH)/nvidia/tegra194/xusb.bin:$(TARGET_COPY_OUT_VENDOR)/firmware/nvidia/tegra194/xusb.bin
+
+
+# Recovery / Boot Ramdisk
+PRODUCT_COPY_FILES += \
+    $(T194_FIRMWARE_PATH)/nvidia/tegra194/xusb.bin:recovery/root/lib/firmware/nvidia/tegra194/xusb.bin \
+    $(T194_FIRMWARE_PATH)/nvidia/tegra194/xusb.bin:$(TARGET_COPY_OUT_RAMDISK)/lib/firmware/nvidia/tegra194/xusb.bin
